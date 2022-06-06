5a761d96dc451b85613678544c974dff7d5b5cd5
jincrates <94jingyu@gmail.com>
2022-06-06 17:01:31 +0900

    kotlin init

---
+++ kotlin-microservices/src/test/kotlin/me/jincrates/kotlinmicroservices/KotlinMicroservicesApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.jincrates.kotlinmicroservices
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinMicroservicesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

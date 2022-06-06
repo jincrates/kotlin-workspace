e6f52557fc78c9b1f84d1cda49b803d450049e06
jincrates <94jingyu@gmail.com>
2022-06-07 08:43:12 +0900

    자바 17버전이 코틀린과 호환되지 않아서 11버전으로 다운그레이드

--- microservices-kotlin/restful-kotlin/src/main/kotlin/me/jincrates/restful/customer/CustomerController.kt
+++ microservices-kotlin/restful-kotlin/src/main/kotlin/me/jincrates/restful/customer/CustomerController.kt
@@ -1,15 +1,27 @@
 package me.jincrates.restful.customer
 
+import me.jincrates.restful.error.ErrorResponse
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class CustomerController {
+    @Autowired
+    private lateinit var customerService: CustomerService
 
-
-    @GetMapping(value = arrayOf("/customer"))
-    fun getCustomer() = Customer(1, "Kotlin");
+    @GetMapping(value = arrayOf("/customer/{id}"))
+    fun getCustomer(@PathVariable id: Int) : ResponseEntity<Any> {
+        val customer = customerService.getCustomer(id);
+        return if (customer != null) {
+            ResponseEntity(customer, HttpStatus.OK)
+        } else {
+            ResponseEntity(ErrorResponse("Customer Not Found", "customer '$id' not found"), HttpStatus.NOT_FOUND);
+        }
+    }
 
 }
--- microservices-kotlin/restful-kotlin/src/main/kotlin/me/jincrates/restful/customer/CustomerServiceImpl.kt
+++ microservices-kotlin/restful-kotlin/src/main/kotlin/me/jincrates/restful/customer/CustomerServiceImpl.kt
@@ -9,29 +9,27 @@ class CustomerServiceImpl : CustomerService {
         private val initialCustomers = arrayOf(
             Customer(1, "Kotlin"),
             Customer(2, "Spring"),
-            Customer(3, "Microservice"))
-        val customers = ConcurrentHashMap<Int, Customer>(initialCustomers.associateBy(Customer::id))
+            Customer(3, "Microservice", Customer.Telephone("+83", "12345678")))
     }
+    val customers = ConcurrentHashMap<Int, Customer>(initialCustomers.associateBy(Customer::id))
 
-    //service 구현하기
-
-    override fun getCustomer(id: Int): Customer? {
-        TODO("Not yet implemented")
-    }
+    override fun getCustomer(id: Int) = customers[id];
 
     override fun createCustomer(customer: Customer) {
-        TODO("Not yet implemented")
+        customers[customer.id] = customer;
     }
 
     override fun deleteCustomer(id: Int) {
-        TODO("Not yet implemented")
+        customers.remove(id);
     }
 
     override fun updateCustomer(id: Int, customer: Customer) {
-        TODO("Not yet implemented")
+        deleteCustomer(id)
+        createCustomer(customer)
     }
 
-    override fun searchCustomers(nameFilter: String): List<Customer> {
-        TODO("Not yet implemented")
-    }
+    override fun searchCustomers(nameFilter: String): List<Customer> =
+        customers.filter {
+            it.value.name.contains(nameFilter, true)
+        }.map(Map.Entry<Int, Customer>::value).toList();
 }

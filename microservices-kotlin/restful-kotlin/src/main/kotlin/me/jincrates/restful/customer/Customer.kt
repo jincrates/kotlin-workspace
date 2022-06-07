457506f7fcc603a0045a1d343ccc308f909ab5bc
jincrates <94jingyu@gmail.com>
2022-06-07 22:58:18 +0900

    널 값 처리

--- microservices-kotlin/restful-kotlin/src/main/kotlin/me/jincrates/restful/customer/Customer.kt
+++ microservices-kotlin/restful-kotlin/src/main/kotlin/me/jincrates/restful/customer/Customer.kt
@@ -1,5 +1,10 @@
 package me.jincrates.restful.customer
 
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonInclude.Include
+
+//yaml에서 설정하여 주석처리
+//@JsonInclude(Include.NON_NULL)
 data class Customer(var id: Int = 0, var name: String = "", var telephone: Telephone? = null) {
     data class Telephone(var countryCode: String = "", var telephoneNumber: String = "")
 }

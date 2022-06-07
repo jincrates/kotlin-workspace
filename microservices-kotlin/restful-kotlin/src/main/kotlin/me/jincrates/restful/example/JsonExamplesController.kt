a8087444074a9a5263ad701d461bb64d4bf00c2a
jincrates <94jingyu@gmail.com>
2022-06-07 22:45:05 +0900

    직렬화 이해 - 간단한 객체 이용

---
+++ microservices-kotlin/restful-kotlin/src/main/kotlin/me/jincrates/restful/example/JsonExamplesController.kt
@@ -0,0 +1,10 @@
+package me.jincrates.restful.example
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class JsonExamplesController {
+    @GetMapping(value = arrayOf("/json"))
+    fun getJson() = SimpleObject("hi", "kotlin")
+}

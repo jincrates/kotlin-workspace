c5a8078c26c52f0b82c6d5a7cee426af4c9313ae
jincrates <94jingyu@gmail.com>
2022-06-07 22:47:36 +0900

    직렬화 이해 - 복잡한 객체 작업

--- microservices-kotlin/restful-kotlin/src/main/kotlin/me/jincrates/restful/example/JsonExamplesController.kt
+++ microservices-kotlin/restful-kotlin/src/main/kotlin/me/jincrates/restful/example/JsonExamplesController.kt
@@ -6,5 +6,6 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class JsonExamplesController {
     @GetMapping(value = arrayOf("/json"))
-    fun getJson() = SimpleObject("hi", "kotlin")
+    //fun getJson() = SimpleObject("hi", "kotlin")
+    fun getJson() = ComplexObject(object1 = SimpleObject("more", "complex"))
 }

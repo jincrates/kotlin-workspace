308a49b2a94bd13e5c8080bd718f2696f728bdc1
jincrates <94jingyu@gmail.com>
2022-06-07 22:11:25 +0900

    @PostMapping

--- microservices-kotlin/restful-kotlin/src/main/kotlin/me/jincrates/restful/customer/CustomerController.kt
+++ microservices-kotlin/restful-kotlin/src/main/kotlin/me/jincrates/restful/customer/CustomerController.kt
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -24,4 +26,9 @@ class CustomerController {
         }
     }
 
+    @PostMapping(value = arrayOf("/customer/"))
+    fun createCustomer(@RequestBody customer: Customer): ResponseEntity<Unit?> {
+        customerService.createCustomer(customer)
+        return ResponseEntity(null, HttpStatus.CREATED)
+    }
 }

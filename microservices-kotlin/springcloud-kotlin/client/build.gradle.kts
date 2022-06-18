95c406f7e42b79cf70a8286b3017fe1d737e6880
jincrates <94jingyu@gmail.com>
2022-06-19 00:04:58 +0900

    microservice client commit

--- microservices-kotlin/springcloud-kotlin/client/build.gradle.kts
+++ microservices-kotlin/springcloud-kotlin/client/build.gradle.kts
@@ -18,11 +18,11 @@ repositories {
 extra["springCloudVersion"] = "2021.0.3"
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.cloud:spring-cloud-starter-config")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 

a6da71d5f431ef6ea612732d8f61a651dd9cf0ec
jincrates <94jingyu@gmail.com>
2022-06-19 00:22:45 +0900

    change port

--- microservices-kotlin/springcloud-kotlin/client/build.gradle.kts
+++ microservices-kotlin/springcloud-kotlin/client/build.gradle.kts
@@ -21,6 +21,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.cloud:spring-cloud-starter-config")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")

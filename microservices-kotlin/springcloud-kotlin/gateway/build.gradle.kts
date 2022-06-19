04d5dc5d1954c35d186206014718333e6b780b8c
jincrates <94jingyu@gmail.com>
2022-06-19 09:26:34 +0900

    게이트웨 설정 zuul 대신 spring cloud gateway로 변경이 필요하다.

--- microservices-kotlin/springcloud-kotlin/gateway/build.gradle.kts
+++ microservices-kotlin/springcloud-kotlin/gateway/build.gradle.kts
@@ -23,6 +23,8 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.springframework.cloud:spring-cloud-starter-config")
 	implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
+	//implementation("org.springframework.cloud:spring-cloud-starter-netflix-zuul")
+	implementation("org.springframework.cloud:spring-cloud-starter-gateway")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 

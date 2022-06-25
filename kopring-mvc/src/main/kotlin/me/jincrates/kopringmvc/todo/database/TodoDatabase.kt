6d34f0e4b5a2828fb69bce5af43507d53682d34a
jincrates <94jingyu@gmail.com>
2022-06-25 16:20:43 +0900

    todo application database setting

---
+++ kopring-mvc/src/main/kotlin/me/jincrates/kopringmvc/todo/database/TodoDatabase.kt
@@ -0,0 +1,11 @@
+package me.jincrates.kopringmvc.todo.database
+
+data class TodoDatabase(
+    var index: Int = 0,
+    var todoLists: MutableList<Todo> = mutableListOf()
+) {
+    fun init() {
+        this.todoLists = mutableListOf()
+        println("[DEBUG] todo database init")
+    }
+}

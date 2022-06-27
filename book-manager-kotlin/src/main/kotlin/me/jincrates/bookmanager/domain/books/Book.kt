5efb5bd86f896751557c4d872b1e2f26ca6a31e3
jincrates <94jingyu@gmail.com>
2022-06-28 08:40:58 +0900

    validation 상세 message 추가

--- book-manager-kotlin/src/main/kotlin/me/jincrates/bookmanager/domain/books/Book.kt
+++ book-manager-kotlin/src/main/kotlin/me/jincrates/bookmanager/domain/books/Book.kt
@@ -18,25 +18,25 @@ data class Book(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     var id: Long?= null,
 
-    @field: NotBlank
+    @field: NotBlank(message = "제목을 입력하지 않았습니다.")
     var title: String?= null,
 
-    @field: NotBlank
+    @field: NotBlank(message = "저자를 입력하지 않았습니다.")
     var author: String?= null,
 
-    @field: NotBlank
+    @field: NotBlank(message = "출판사를 입력하지 않았습니다.")
     var publisher: String?= null,
 
     @field: StringFormatDateTime(pattern = "yyyy-MM-dd HH:mm:ss", message = "yyyy-MM-dd HH:mm:ss 포맷이 맞지 않습니다.")
     var publicationDate: String?= null,
 
-    @field: NotBlank
+    @field: NotBlank(message = "ISBN을 입력하지 않았습니다.")
     var isbn: String?= null,
 
     var imagePath: String?= null,
 
-    @field: NotNull
-    @field: PositiveOrZero  // 양수 또는 0이어야 합니다.
+    @field: NotNull(message = "수량을 입력하지 않았습니다.")
+    @field: PositiveOrZero(message = "수량은 0 이상이어야 합니다.")  // 양수 또는 0이어야 합니다.
     var quantity: Int?= null
 
 ) : BaseEntity()

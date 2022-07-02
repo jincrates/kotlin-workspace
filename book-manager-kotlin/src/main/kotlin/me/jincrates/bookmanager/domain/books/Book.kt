e6341475e3ad76a991fafa9ceb03920fdf627c45
jincrates <94jingyu@gmail.com>
2022-07-02 09:05:33 +0900

    엔티티 수정

--- book-manager-kotlin/src/main/kotlin/me/jincrates/bookmanager/domain/books/Book.kt
+++ book-manager-kotlin/src/main/kotlin/me/jincrates/bookmanager/domain/books/Book.kt
@@ -13,28 +13,29 @@ import javax.validation.constraints.PositiveOrZero
 data class Book(
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var id: Long?= null,
+    @Column(name = "book_id")
+    var id: Long? = null,
 
     @field: NotBlank(message = "제목을 입력하지 않았습니다.")
-    var title: String?= null,
+    var title: String? = null,
 
     @field: NotBlank(message = "저자를 입력하지 않았습니다.")
-    var author: String?= null,
+    var author: String? = null,
 
     @field: NotBlank(message = "출판사를 입력하지 않았습니다.")
-    var publisher: String?= null,
+    var publisher: String? = null,
 
     @field: StringFormatDateTime(pattern = "yyyy-MM-dd HH:mm:ss", message = "yyyy-MM-dd HH:mm:ss 포맷이 맞지 않습니다.")
-    var publicationDate: String?= null,
+    var publicationDate: String? = null,
 
     @field: NotBlank(message = "ISBN을 입력하지 않았습니다.")
-    var isbn: String?= null,
+    var isbn: String? = null,
 
-    var imagePath: String?= null,
+    var imagePath: String? = null,
 
     @field: NotNull(message = "수량을 입력하지 않았습니다.")
     @field: PositiveOrZero(message = "수량은 0 이상이어야 합니다.")  // 양수 또는 0이어야 합니다.
-    var quantity: Int?= null
+    var quantity: Int? = null
 
 ) : BaseEntity()
 

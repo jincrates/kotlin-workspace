b049c1a7f2fa98c77605ecdf603ba48a3bcd65b7
jincrates <94jingyu@gmail.com>
2022-07-31 12:37:33 +0900

    BaseEntity 엔티티 재설정

--- book-manager-kotlin/src/main/kotlin/me/jincrates/bookmanager/domain/BaseEntity.kt
+++ book-manager-kotlin/src/main/kotlin/me/jincrates/bookmanager/domain/BaseEntity.kt
@@ -1,23 +1,20 @@
 package me.jincrates.bookmanager.domain
 
-import lombok.Getter
-import org.springframework.data.annotation.CreatedBy
-import org.springframework.data.annotation.LastModifiedBy
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
-import javax.persistence.Column
 import javax.persistence.EntityListeners
 import javax.persistence.MappedSuperclass
 
-@Getter
+// TODO: @MappedSuperclass?
 @MappedSuperclass
-@EntityListeners(value = [AuditingEntityListener::class])
-open class BaseEntity : BaseTimeEntity() {
+@EntityListeners(AuditingEntityListener::class)
+abstract class BaseEntity(
 
-    @CreatedBy
-    @Column(updatable = false)
-    lateinit var createdBy: String
+    @CreatedDate
+    var createdAt: LocalDateTime? = null,
 
-    @LastModifiedBy
-    lateinit var updatedBy: String
-}
+    @LastModifiedDate
+    var updatedAt: LocalDateTime? = null,
+)

789e87e23bcd720aa645d55bcc6f4b7871dc29fa
jincrates <94jingyu@gmail.com>
2022-07-31 10:18:50 +0900

    코멘트 수정

--- issue-service-kotlin/issue-service/src/main/kotlin/me/jincrates/issueservicekotlin/domain/CommentRepository.kt
+++ issue-service-kotlin/issue-service/src/main/kotlin/me/jincrates/issueservicekotlin/domain/CommentRepository.kt
@@ -3,4 +3,5 @@ package me.jincrates.issueservicekotlin.domain
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface CommentRepository : JpaRepository<Comment, Long> {
+    fun findByIdAndUserId(id: Long, userId: Long) : Comment?
 }

3f5fa9557c5499c70f76574511e49d37ec98d0e9
jincrates <94jingyu@gmail.com>
2022-08-13 10:13:56 +0900

    재고시스템으로 알아보는 동시성 이슈

--- concurrency-problems-of-stock/src/test/kotlin/me/jincrates/stock/StockApplicationTests.kt
+++ concurrency-problems-of-stock/src/test/kotlin/me/jincrates/stock/StockApplicationTests.kt
@@ -1,10 +1,10 @@
-package me.jincrates.bookmanager
+package me.jincrates.stock
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class BookManagerApplicationTests {
+class StockApplicationTests {
 
 	@Test
 	fun contextLoads() {

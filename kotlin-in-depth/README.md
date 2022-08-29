f30bf7969974719cdd29e2e137eb9cbc11eb9f5d
jincrates <94jingyu@gmail.com>
2022-08-30 00:02:50 +0900

    코틀린 클래스

--- kotlin-in-depth/README.md
+++ kotlin-in-depth/README.md
@@ -1,5 +1,5 @@
 # 코틀린 완벽 가이드
-http://aladin.kr/p/xfWwP
+https://github.com/gilbutITbook/080250
 - 저자: 알렉세이 세두노프 (Aleksei Sedunov)
 - 역자: 오현석
 - 출판사: 길벗(2022)
---
+++ kotlin-in-depth/src/main/kotlin/me/jincrates/kotlinindepth/chapter04/Person2.kt
@@ -0,0 +1,11 @@
+package me.jincrates.kotlinindepth.chapter04
+
+//4.1.2 생성자
+class Person2(firstName: String, familyName: String) {
+    val fullName = "$firstName $familyName"
+}
+
+fun main() {
+    val person = Person2("길동", "홍")
+    println(person.fullName)
+}

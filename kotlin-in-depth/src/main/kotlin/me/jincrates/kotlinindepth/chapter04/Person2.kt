0e0e30e62c01dbd80a09286a1fe913f4d26a989e
jincrates <94jingyu@gmail.com>
2022-08-31 15:02:52 +0900

    생성자 클래스 인스턴스

--- kotlin-in-depth/src/main/kotlin/me/jincrates/kotlinindepth/chapter04/Person2.kt
+++ kotlin-in-depth/src/main/kotlin/me/jincrates/kotlinindepth/chapter04/Person2.kt
@@ -3,6 +3,10 @@ package me.jincrates.kotlinindepth.chapter04
 //4.1.2 생성자
 class Person2(firstName: String, familyName: String) {
     val fullName = "$firstName $familyName"
+
+    init {
+        println("Created new Person instance: $fullName")
+    }
 }
 
 fun main() {
